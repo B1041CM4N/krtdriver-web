8e51881653c01d5bba12f5aae233955fc2af0eb1
Ignacio Andrés Rodríguez Chandía <rodriguezch_ignacioandres@hotmail.cl>
2017-06-03 01:49:07 -0400

    V3062017

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+User.create(
+    email: "ignacio@krt.cl",
+    password: "123123123",
+    password_confirmation: "123123123",
+    status: 1
+)
+
+User.create(
+    email: "ramon@krt.cl",
+    password: "123123123",
+    password_confirmation: "123123123",
+    status: 1
+)
+
+User.create(
+    email: "gabriel@krt.cl",
+    password: "123123123",
+    password_confirmation: "123123123",
+    status: 1
+
+)
