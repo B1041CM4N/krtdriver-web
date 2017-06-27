a65d537dbc074a47cf7f3f733823507fd4aee51f
Ignacio Andrés Rodríguez Chandía <rodriguezch_ignacioandres@hotmail.cl>
2017-06-27 17:53:23 -0400

    test assets 3

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,8 +12,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
+  config.mailer_sender = 'no-reply@krt.cl'
+  config.secret_key = '25126dc0445f269d2226be62fee4eb337497f750d4ef89cc4cc4e1c203c4847faad9ccff29fd6adfe9ecfebb36bb4e0e4b8e12e018a5367641e8de490322cdd4'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
