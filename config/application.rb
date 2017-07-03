7856a7f8f8dc913b25a62a6a72251aa4b8793946
Ignacio Andrés Rodríguez Chandía <rodriguezch_ignacioandres@hotmail.cl>
2017-07-03 02:32:58 -0400

    arreglos variados, devise

--- config/application.rb
+++ config/application.rb
@@ -13,10 +13,11 @@ module KrtdriverWeb
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    
     require 'i18n/backend/fallbacks'
     I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
     config.i18n.fallbacks = { 'es' => 'en' }
     I18n.locale = :es
+
+    config.logger = Logger.new(STDOUT)
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -198,7 +198,7 @@ Devise.setup do |config|
   # ==> Configuration for :recoverable
   #
   # Defines which key will be used when recovering the password for an account
-  # config.reset_password_keys = [:email]
+  config.reset_password_keys = [:email]
 
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to
@@ -231,7 +231,7 @@ Devise.setup do |config|
 
   # Set this configuration to false if you want /users/sign_out to sign out
   # only the current scope. By default, Devise signs out all scopes.
-  # config.sign_out_all_scopes = true
+  config.sign_out_all_scopes = true
 
   # ==> Navigation configuration
   # Lists the formats that should be treated as navigational. Formats like
