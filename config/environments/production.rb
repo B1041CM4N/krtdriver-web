6a2e5d3a422e2c79693701beba23696f8643f7e3
Ignacio Andrés Rodríguez Chandía <rodriguezch_ignacioandres@hotmail.cl>
2017-07-09 19:27:14 -0400

    FIX: production environment settings for mailtrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   ##DEFAULT URL FOR DEVISE
-  config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
+  config.action_mailer.default_url_options = { host: 'https://krtdriver-web.herokuapp.com', port: 46851 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => 'a7e4f61aa8844f',
